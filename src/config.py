0bb8d3a3e0afbe28ba3ef971431492b7758422ad
metbatgui-dev <metbatguidev@gmail.com>
2025-12-03 13:18:00 +0900

    🚀 chore: 프로젝트 설정 및 기본 구조 추가
    - pyproject.toml, uv.lock: 의존성 관리 설정
    - .gitignore, .python-version: 프로젝트 환경 설정
    - src/config.py: 애플리케이션 설정 클래스 추가

---
+++ src/config.py
@@ -0,0 +1,6 @@
+class Config:
+    START_HOUR = 7
+    END_HOUR = 18
+    CHECK_INTERVAL = 60  # seconds
+    TARGET_URL = "https://www.newspim.com/search?searchword=리포트+브리핑"
+    KEYWORD = "리포트 브리핑"
