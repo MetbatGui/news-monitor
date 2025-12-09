66996dc3a98266549e887296df3a95c15279bec4
metbatgui-dev <metbatguidev@gmail.com>
2025-12-09 14:42:52 +0900

    🔥 chore: RSS 전환 완료된 웹 스크래퍼 제거
    - 6개 웹 스크래퍼를 RSS로 완전히 전환하여 제거
    
    Deleted:
    - [DELETE] asiae_scraper.py (아시아경제)
    - [DELETE] etoday_scraper.py (이투데이)
    - [DELETE] hankyung_scraper.py (한국경제)
    - [DELETE] newspim_scraper.py (뉴스핌)
    - [DELETE] seoul_scraper.py (서울경제)
    - [DELETE] yonhap_scraper.py (연합뉴스)
    
    Remaining Web Scrapers:
    - FnScraper (파이낸셜뉴스)
    - MTScraper (머니투데이)
    
    Changes:
    - [MODIFY] web/__init__.py

--- src/adapters/infrastructure/scrapers/web/__init__.py
+++ src/adapters/infrastructure/scrapers/web/__init__.py
@@ -1,11 +1,9 @@
 """웹 스크래핑 기반 뉴스 스크래퍼 모음"""
 
-from adapters.infrastructure.scrapers.web.hankyung_scraper import HankyungScraper
+from adapters.infrastructure.scrapers.web.fn_scraper import FnScraper
 from adapters.infrastructure.scrapers.web.mt_scraper import MTScraper
-from adapters.infrastructure.scrapers.web.newspim_scraper import NewspimScraper
 
 __all__ = [
-    'HankyungScraper',
+    'FnScraper',
     'MTScraper',
-    'NewspimScraper',
 ]
